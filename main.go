6e7587b8ec11b2887ed7ea7aa8d10c745befedda
arvinkai <ywy_sky@foxmail.com>
2015-11-24 01:21:01 +0800

    test2

--- main.go
+++ main.go
@@ -8,6 +8,6 @@ import (
 
 func main() {
 	db := sql.Open("mysql", "root/arvin@tcp(localhost:3306)/test?cha-utf-8")
-	fmt.Println("Hello World!")
+	fmt.Println("Hello World!  123123123")
 	fmt.Println("This's ok!!!")
 }
