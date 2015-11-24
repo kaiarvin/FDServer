eab763cef0ee5e28abb8cffd3175a4a013c020e0
arvinkai <ywy_sky@foxmail.com>
2015-11-25 00:35:40 +0800

    Create struct

--- ChatServer.go
+++ ChatServer.go
@@ -1,1 +1,23 @@
 // ChatServer
+package ChatServer
+
+type Client struct {
+	ID       int
+	Name     string
+	Icon     int
+	Level    int
+	Area     int
+	Viplevel int
+	Msg      chan string
+	UserRelation
+}
+
+type UserRelation struct {
+	FriendInfo map[uint64]string
+	IgnorID    map[uint64]string
+}
+
+type ChatServer struct {
+	ID          int
+	Client_Pool []Client
+}
