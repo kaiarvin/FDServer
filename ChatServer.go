1486b0eedaf4f1d3c1407163043eadea7e868be7
arvinkai <ywy_sky@foxmail.com>
2015-12-01 18:14:46 +0800

    InitChatServer

--- ChatServer.go
+++ ChatServer.go
@@ -6,19 +6,21 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net"
 	"os"
 	"strings"
 )
 
 type Client struct {
-	AccountID    int
-	Name         string
-	Icon         int
-	Level        int
-	Area         int
-	Viplevel     int
-	RecMsg       chan string
-	ChatChannlID int
+	AccountID     int
+	Name          string
+	Icon          int
+	Level         int
+	Area          int
+	Viplevel      int
+	RecMsg        chan string
+	ChatChannlID  int
+	Client_Socket net.Conn
 	UserRelation
 }
 
@@ -28,13 +30,14 @@ type UserRelation struct {
 }
 
 type ChatServer struct {
-	ID             int               //服务器ID
-	Port           int               //监听Client端口号
-	Host           string            //host地址
-	UserList       map[uint64]Client //用户列表
-	ChannlList     map[int]chan byte //通道列表
-	DataChannl     chan byte         //中转接受数据
-	ChatConfigData map[string]string
+	ID               int                 //服务器ID
+	Port             int                 //监听Client端口号
+	Host             string              //host地址
+	UserList         map[net.Conn]Client //用户列表
+	ChannlList       map[int]chan byte   //通道列表
+	ChannlList_Count []int               //统计Channlist_count里面各个Channl的用户数
+	DataChannl       chan byte           //中转接受数据
+	ChatConfigData   map[string]string
 }
 
 func (this *ChatServer) ProcessConf(args []string) {
@@ -47,7 +50,7 @@ func (this *ChatServer) ProcessConf(args []string) {
 }
 
 func (this *ChatServer) ReadConf(name string, handler func([]string)) (err error) {
-	this.initChatServerData()
+	this.InitChatServerData()
 	f, err := os.Open(name)
 	if err != nil {
 		log.Fatalln(err)
@@ -71,17 +74,40 @@ func (this *ChatServer) ReadConf(name string, handler func([]string)) (err error
 	return nil
 }
 
-func (this *ChatServer) initChatServerData() {
+func (this *ChatServer) InitChatServerData() {
 	this.ID = 0
 	this.Port = 0
 	this.Host = ""
 	this.DataChannl = make(chan byte, 1024)
 	this.ChatConfigData = make(map[string]string, 0)
-	this.UserList = make(map[uint64]Client)
-	this.ChannlList = make(map[int]chan byte)
+	this.UserList = make(map[net.Conn]Client, 100)
+	this.ChannlList = make(map[int]chan byte, 20)
 }
 
-func (this *ChatServer) InitServer() {
+func (this *ChatServer) InitServer() error {
+	server, err := net.Listen("TCP4", "localhost:8085")
+	defer server.Close()
+
+	if err != nil {
+		return err
+	}
+
+	for {
+		client_socket, err := server.Accept()
+		if err != nil {
+			return err
+		}
+		this.NewClient(client_socket)
+	}
+
+	return nil
+}
+
+func (this *ChatServer) NewClient(n net.Conn) {
+	client := &Client{Client_Socket: n}
+	this.UserList[n] = client
+
+	//分配channl统计channl count
 
 }
 
